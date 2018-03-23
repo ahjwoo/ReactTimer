f5314eb21558f137315f3caa48d2390390921585
Andrew Woo <ahjwoo@gmail.com>
2018-03-22 22:47:33 -0700

    support foundation settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
   entry: [
@@ -48,6 +49,11 @@ module.exports = {
         loader: 'url?limit=25000',
       },
     ],
+  },
+  sassLoader: {
+    includePaths: [
+      path.resolve(__dirname, './node_modules/foundation-sites/scss')
+    ]
   },
   devtool: 'cheap-module-eval-source-map'
 };
